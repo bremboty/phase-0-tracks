9c1c2aa8b239b9e15605fbc2929d3cfd2c05f929
Tyler Levad <tylerlevad23@gmail.com>
2016-10-12 06:22:39 -0500

    Do 4.4 release 3

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,39 +1,68 @@
-puts "What is your name?"
-name = gets.chomp.to_s
+puts "How many employees will be processed?"
+number_of_employees = gets.chomp.to_i
+variable = 0
 
-puts "How old are you?"
-age = gets.chomp.to_i
+until variable == number_of_employees
+	
+	#That whole thing
+	
+	puts "What is your name?"
+	name = gets.chomp.to_s
 
-puts "What year were you born?"
-birth_year = gets.chomp.to_i
+	puts "How old are you?"
+	age = gets.chomp.to_i
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you y/n?"
-garlic_bread = gets.chomp.to_s
+	puts "What year were you born?"
+	birth_year = gets.chomp.to_i
 
-until (garlic_bread == "y" or garlic_bread == "n")
-	puts "Please use y or n"
+	if (2016 - birth_year == age)
+		age_right = true
+	else
+		age_right = false
+	end
+
+	puts "Our company cafeteria serves garlic bread. Should we order some for you y/n?"
 	garlic_bread = gets.chomp.to_s
+
+	until (garlic_bread == "y" or garlic_bread == "n")
+		puts "Please use y or n"
+		garlic_bread = gets.chomp.to_s
+	end
+
+	if garlic_bread == "y"
+		eats_garlic = true
+	else
+		eats_garlic = false
+	end
+
+	puts "Would you like to enroll in the company’s health insurance y/n?"
+	insurance = gets.chomp.to_s
+
+	until (insurance == "y" or insurance == "n")
+		puts "Please use y or n"
+		insurance = gets.chomp.to_i
+	end
+
+	if insurance == "y"
+		wants_insurance = true
+	else
+		wants_insurance = false
+	end
+
+
+
+	if (name == "Drake Cula" or name == "Tu Fang")
+		puts "Definitely a vampire."
+	elsif !age_right && !eats_garlic && !wants_insurance
+		puts "Almost certainly a vampire."
+	elsif !age_right && (!eats_garlic || !wants_insurance)
+		puts "Probably a vampire"
+	elsif age_right && (eats_garlic || wants_insurance)
+		puts "Probably not a vampire."
+	else
+		puts "Results inconclusive."
+	end
+	
+	variable += 1 
+	
 end
-
-if garlic_bread == "y"
-	eats_garlic = true
-else
-	eats_garlic = false
-end
-
-
-puts "Would you like to enroll in the company’s health insurance y/n?"
-insurance = gets.chomp.to_s
-
-until (insurance == "y" or insurance == "n")
-	puts "Please use y or n"
-	insurance = gets.chomp.to_i
-end
-
-if insurance == "y"
-	wants_insurance = true
-else
-	wants_insurance = false
-end
-
-
