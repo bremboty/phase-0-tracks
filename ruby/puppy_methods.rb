11f1072b4c74e5d701f96fea8025203354dcb796
Tyler Levad <tylerlevad23@gmail.com>
2016-10-23 09:52:59 -0500

    Finish 6.2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -38,8 +38,62 @@ class Puppy
 		p "*throws treat*"
 	end
 
+	# Add a method to your Puppy class named initialize. It should print 
+	# "Initializing new puppy instance ..."
+	def initialize
+		p "Initializing new puppy instance ..."
+	end
 
 end
+
+# design and implement your own class
+class Car
+
+
+	# it should have an initialize method 
+	attr_accessor :num
+    def initialize (num)
+        @num = num
+    end
+
+	# and at least two other instance methods
+	def start_car
+		p "ch.chh.chhhvvv.vv..VROOMMM! rumble rumble rumble..."
+	end
+
+	def fill_up(gallons)
+		gallons.to_i
+		p "I put #{gallons} gallons in car#{@num}"
+	end
+
+end
+
+# Use a loop to make 50 instances of your class.
+cars = (1..50).collect do |i|
+	Car.new(i)
+end
+
+# Modify your loop so that it stores all of the instances in a data structure.
+
+# Iterate over that data structure using .each and call the instance methods you wrote on each 
+# instance. So if you wrote a Gymnast class, this is where you'd call .flip and .jump on
+# your instances of Gymnast.
+
+cars.each do |car|
+	car.start_car
+	car.fill_up(5)
+end
+	
+
+
+
+
+
+
+
+
+
+
 
 # TEST CODE
 iroh = Puppy.new
@@ -48,6 +102,13 @@ iroh.speak(8)
 iroh.roll_over
 iroh.dog_years(8)
 iroh.feed_treat
+
+mazda = Car.new("mazda")
+mazda.fill_up(12)
+mazda.start_car
+
+
+
 
 
 
