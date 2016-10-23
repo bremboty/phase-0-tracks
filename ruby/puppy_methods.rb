f9c65d659438e5db50b4f8026d4389e5c91074a1
Tyler Levad <tylerlevad23@gmail.com>
2016-10-23 08:12:39 -0500

    did 6.2 r1

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,16 +1,55 @@
 # driver code
-
 class Puppy
+  
+    def fetch(toy)
+      puts "I brought back the #{toy}!"
+      toy
+    end
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+
+	# Add a speak method that takes an integer, and then prints "Woof!" that many times.
+	def speak(num)
+		num.to_i
+		num.times do |i|
+			p "Woof!"
+		end
+	end
+
+
+	# Add a roll_over method that just prints "*rolls over*".
+	def roll_over
+		p "*rolls over*"
+	end
+
+	
+
+	# Add a dog_years method that takes an integer (of human years) and converts 
+	# that number to dog years, returning a new integer.
+
+	def dog_years(num)
+		num.to_i
+		dog_age = num * 7
+		p "Your dog is #{dog_age}"
+	end
+
+
+	# method that takes in a name and says #name is dead
+	def feed_treat
+		p "*throws treat*"
+	end
+
 
 end
 
+# TEST CODE
 iroh = Puppy.new
 iroh.fetch("stick")
+iroh.speak(8)
+iroh.roll_over
+iroh.dog_years(8)
+iroh.feed_treat
+
+
 
 
 # User Interface
