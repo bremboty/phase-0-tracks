0dc92f72101e7e06ea616f89eec82b57d33b9cb6
Tyler Levad <tylerlevad23@gmail.com>
2016-10-23 07:21:22 -0500

    6.1 r0

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+#	SPECIES ------------------------
+#	Puppyus Cutuss
+#
+#	Paw Count: 4
+#	Friendly: yes
+#	Eye count: 2
+#	Name: varies
+#	Color: varies
+#   tail count: 1
+#   
+#
+#	BEHAVIOR -----------------------
+#	Jump
+#	Hug
+#	Scamper
+#   fetch
+#   roll over
