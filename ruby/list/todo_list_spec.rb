3a81ff52f69d7beed616520dc831242c3bfc7873
Tyler Levad <tylerlevad23@gmail.com>
2016-10-24 04:49:14 -0500

    Turn in 6.5 r3

--- ruby/list/todo_list_spec.rb
+++ ruby/list/todo_list_spec.rb
@@ -1,3 +1,17 @@
+=begin
+In phase-0-tracks/ruby/list, you'll find a test file for a 
+TodoList class. The class itself isn't anything fancy -- it 
+basically maintains an internal array attribute and provides 
+methods that allow the array to be accessed and updated in various 
+ways.
+
+Run the tests. Resolve any errors you see and add code that 
+will make all of the tests pass. Work on one test at a time 
+until you've made it all the way to "green" (all tests passing).
+=end
+
+require_relative 'todo_list'
+
 describe TodoList do
   let(:list) { TodoList.new(["do the dishes", "mow the lawn"]) }
 
